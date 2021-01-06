13926bb62662992876d4510508d7cca86a7eede3
Juan F. Escobar <juanfesba@hotmail.com>
2021-01-05 22:29:09 -0500

    more setup

--- src/kaku_app.py
+++ src/kaku_app.py
@@ -1,3 +1,15 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+from . import routing  # For import side-effects of setting up routes.
+import os
+
+app.config.from_mapping(
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+)
+
+'''
+socketio = SocketIO(app)
+socketio.run(app)
+#if __name__ == '__main__':
+    #socketio.run(app)
+'''
