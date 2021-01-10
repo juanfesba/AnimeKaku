a3818410ebdb7eaf9d9bd7fb55644d68a35d41a1
Juan F. Escobar <juanfesba@hotmail.com>
2021-01-10 05:48:48 -0500

    starting socket support

--- src/home_page.py
+++ src/home_page.py
@@ -1,7 +1,8 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, make_response
 
 bp = Blueprint('home_page', __name__)
 
 @bp.route("/")
 def home():
-    return render_template("home.html", name=None)
+    response = make_response(render_template('home.html', name=None))
+    return response
--- src/kaku_app.py
+++ src/kaku_app.py
@@ -4,7 +4,10 @@ from flask_socketio import SocketIO
 import os
 
 app.config.from_mapping(
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY'),
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
 )
 
 from . import home_page
