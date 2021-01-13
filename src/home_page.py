865949a3eb025f0a1297f93d98077748a7499c6c
Juan F. Escobar <juanfesba@hotmail.com>
2021-01-13 18:54:41 -0500

    session tests

--- src/home_page.py
+++ src/home_page.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, make_response, request, flash
+from flask import render_template, Blueprint, make_response, request, flash, session, redirect, url_for
 
 bp = Blueprint('home_page', __name__)
 
@@ -8,7 +8,7 @@ def home():
         input_player_name = request.form.get('input_player_name') #TODO: make this type of checks with a helper function
         if input_player_name is None:
             return "Did not receive any player name :c"
-        return request.form.get('input_player_name')
-
+        session["user_id"] = input_player_name
+        return redirect(url_for('home_page.home'))
     response = make_response(render_template('home.html', name=None))
     return response
