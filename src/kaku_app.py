ad5a1d0d0e86f0750ad6acd7111a96afa05c200a
Juan F. Escobar <juanfesba@hotmail.com>
2021-01-12 23:36:10 -0500

    allow dotenv

--- src/kaku_app.py
+++ src/kaku_app.py
@@ -1,10 +1,13 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command.
 from flask_socketio import SocketIO
-import os
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv('.env')
 
 app.config.from_mapping(
-    SECRET_KEY=os.environ.get('SECRET_KEY'),
+    SECRET_KEY=environ.get('SECRET_KEY'),
     SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_HTTPONLY=True,
     SESSION_COOKIE_SAMESITE='Lax',
