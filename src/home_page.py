ad95fcb492fce86df9905d168ea5879ea065982c
Juan F. Escobar <juanfesba@hotmail.com>
2021-01-12 21:14:48 -0500

    initial login features

--- src/home_page.py
+++ src/home_page.py
@@ -1,8 +1,14 @@
-from flask import render_template, Blueprint, make_response
+from flask import render_template, Blueprint, make_response, request, flash
 
 bp = Blueprint('home_page', __name__)
 
-@bp.route("/")
+@bp.route("/", methods=("GET", "POST"))
 def home():
+    if request.method == "POST":
+        input_player_name = request.form.get('input_player_name') #TODO: make this type of checks with a helper function
+        if input_player_name is None:
+            return "Did not receive any player name :c"
+        return request.form.get('input_player_name')
+
     response = make_response(render_template('home.html', name=None))
     return response
