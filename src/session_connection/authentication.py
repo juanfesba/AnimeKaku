f8800f22348c3c0c2f9b2f27a2a4ff9e3c4f93c3
Juan F. Escobar <juanfesba@hotmail.com>
2021-02-04 03:05:56 -0500

    room setup

--- src/session_connection/authentication.py
+++ src/session_connection/authentication.py
@@ -1,10 +1,11 @@
 from flask import g, session
 
-player_id_gen = "0"
+player_id_gen = 0
 
 def load_logged_in_user():
     player_name = session.get("player_name")
     player_id = session.get("player_id")
+    room_id = session.get("room_id")
 
     if player_name is None:
         g.player_name = None
@@ -12,7 +13,14 @@ def load_logged_in_user():
         g.player_name = player_name
 
     if player_id is None:
-        g.player_id = 0
+        g.player_id = None
+    else:
+        g.player_id = player_id
+
+    if room_id is None:
+        g.room_id = None
+    else:
+        g.room_id = None
 
 def generatePlayerID():
     global player_id_gen
