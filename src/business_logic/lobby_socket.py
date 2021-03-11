c001c3054a440485096c0acf4df2d1feda1ffcfa
Juan F. Escobar <juanfesba@hotmail.com>
2021-03-11 06:35:52 -0500

    nit standard

--- src/business_logic/lobby_socket.py
+++ src/business_logic/lobby_socket.py
@@ -10,7 +10,7 @@ from src.session_connection import authentication
 
 socketio = kaku_app.socketio
 
-def redirectOut(error_message):
+def redirectOutLobby(error_message):
     emit('Redirect Out')
     return error_message
 
@@ -24,11 +24,11 @@ def beforeAppRequest():
 def connectToLobby(data=None):
     err = beforeAppRequest()
     if err is not None: # TODO: flash error
-        redirectOut(err)
+        redirectOutLobby(err)
         return
 
     if 'room_id' not in data:
-        redirectOut("Something happened with your data.")
+        redirectOutLobby("Something happened with your data.")
         return
 
     player_sid = request.sid
@@ -37,12 +37,12 @@ def connectToLobby(data=None):
     lobby = common_helpers.retrieveRoomFromID(room_id)
 
     if lobby is None:
-        redirectOut("Lobby not found in the internet.")
+        redirectOutLobby("Lobby not found in the internet.")
         return
 
     lobby_conf = lobby.lobby_conf
     if data_integrity.dictIsCorrupted(['category_name', 'host_id'], lobby_conf):
-        redirectOut("The data was corrupted :c. Please reload the page.")
+        redirectOutLobby("The data was corrupted :c. Please reload the page.")
 
     player_id = g.player_id
     host_id = lobby_conf['host_id']
@@ -54,12 +54,12 @@ def connectToLobby(data=None):
 
     # Here comes the synchro critical section.
     if player_id in global_state.SESSIONS_TO_CAT_ROOM_IDS:
-        redirectOut("You are already in a lobby/game.")
+        redirectOutLobby("You are already in a lobby/game.")
         return
 
     if lobby.lobby_nature == lobby_logic.LobbyNature.CREATE_LOBBY:
         if not is_host:
-            redirectOut("How did you get here this fast? The host isn't even here yet.")
+            redirectOutLobby("How did you get here this fast? The host isn't even here yet.")
             return
 
         global_state.SOCKETS_TO_SESSIONS[player_sid] = player_id
@@ -68,7 +68,7 @@ def connectToLobby(data=None):
         time.sleep(1.4)
         if player_id not in global_state.SESSIONS_TO_CAT_ROOM_IDS or 'garbage_collector' not in lobby_conf:
             del global_state.SOCKETS_TO_SESSIONS[player_sid]
-            redirectOut("Looks like the internet is not waiting for us.")
+            redirectOutLobby("Looks like the internet is not waiting for us.")
             return
 
         garbage_collector = lobby_conf['garbage_collector']
@@ -78,7 +78,7 @@ def connectToLobby(data=None):
         if garbage_collector.is_alive() or common_helpers.retrieveRoomFromID(room_id) is None:
             del global_state.SOCKETS_TO_SESSIONS[player_sid]
             del global_state.SESSIONS_TO_CAT_ROOM_IDS[player_id]
-            redirectOut("Looks like the internet is not waiting for us.")
+            redirectOutLobby("Looks like the internet is not waiting for us.")
             return
 
         del lobby_conf['garbage_collector']
